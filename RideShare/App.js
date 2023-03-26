ec54c04b4eafbab607c1fb8480217e207477448e
Shubham Bhadoria <84638757+DrGrenkenstien@users.noreply.github.com>
2023-03-26 16:10:14 +0530

    started doing something!

---
+++ RideShare/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder="Enter Contact No.!!"/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
